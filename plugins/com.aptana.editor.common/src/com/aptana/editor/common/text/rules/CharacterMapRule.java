aff336e60acd545dbbcfb63912c9a288024972b1
Kevin Lindsey <klindsey@aptana.com>
2010-09-24 09:21:48 -0500

    Move CharacterMapRule into the common editor plugin so we can use it in more places

--- plugins/com.aptana.editor.common/src/com/aptana/editor/common/text/rules/CharacterMapRule.java
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/text/rules/CharacterMapRule.java
@@ -32,7 +32,7 @@
  * 
  * Any modifications to this file must keep this entire header intact.
  */
-package com.aptana.editor.js.text.rules;
+package com.aptana.editor.common.text.rules;
 
 import java.util.HashMap;
 import java.util.Map;
