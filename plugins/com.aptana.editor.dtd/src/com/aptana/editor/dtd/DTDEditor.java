6c1e0164f80eaeab18b633eade268f4b6a57e134
Kevin Lindsey <klindsey@aptana.com>
2010-09-28 09:10:24 -0500

    Hook a file service for the DTD editor

--- plugins/com.aptana.editor.dtd/src/com/aptana/editor/dtd/DTDEditor.java
+++ plugins/com.aptana.editor.dtd/src/com/aptana/editor/dtd/DTDEditor.java
@@ -1,14 +1,29 @@
 package com.aptana.editor.dtd;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
+import com.aptana.editor.common.parsing.FileService;
+import com.aptana.editor.dtd.parsing.DTDParserConstants;
 
 public class DTDEditor extends AbstractThemeableEditor
 {
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.AbstractThemeableEditor#initializeEditor()
+	 */
 	protected void initializeEditor()
 	{
 		super.initializeEditor();
 		
 		this.setSourceViewerConfiguration(new DTDSourceViewerConfiguration(this.getPreferenceStore(), this));
 		this.setDocumentProvider(new DTDDocumentProvider());
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.AbstractThemeableEditor#createFileService()
+	 */
+	protected FileService createFileService()
+	{
+		return new FileService(DTDParserConstants.LANGUAGE);
 	}
 }
