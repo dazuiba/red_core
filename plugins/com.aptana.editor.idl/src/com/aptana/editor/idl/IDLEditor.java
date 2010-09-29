a7a3512b978842f8a05b356831b58ff23d90cbde
Kevin Lindsey <klindsey@aptana.com>
2010-09-29 15:05:32 -0500

    Wire up a file service with the IDL editor so we can start getting parsing services

--- plugins/com.aptana.editor.idl/src/com/aptana/editor/idl/IDLEditor.java
+++ plugins/com.aptana.editor.idl/src/com/aptana/editor/idl/IDLEditor.java
@@ -1,9 +1,20 @@
 package com.aptana.editor.idl;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
+import com.aptana.editor.common.parsing.FileService;
+import com.aptana.editor.idl.parsing.IDLParserConstants;
 
 public class IDLEditor extends AbstractThemeableEditor
 {
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.AbstractThemeableEditor#createFileService()
+	 */
+	protected FileService createFileService()
+	{
+		return new FileService(IDLParserConstants.LANGUAGE);
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.aptana.editor.common.AbstractThemeableEditor#initializeEditor()
