d75bada2ea37d3bf7949179a723b037aab06d6d4
Kevin Lindsey <klindsey@aptana.com>
2010-10-05 13:56:30 -0500

    Add more unit tests for element declarations. Fix reference to current node to acquire it from the last node instead of from the root node each time.

--- tests/com.aptana.editor.dtd.tests/src/com/aptana/editor/dtd/parsing/DTDParserTests.java
+++ tests/com.aptana.editor.dtd.tests/src/com/aptana/editor/dtd/parsing/DTDParserTests.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import junit.framework.TestCase;
 import beaver.Parser.Exception;
 
-import com.aptana.editor.dtd.parsing.ast.DTDElementDeclarationNode;
+import com.aptana.editor.dtd.parsing.ast.DTDElementDeclNode;
+import com.aptana.editor.dtd.parsing.ast.DTDElementNode;
 import com.aptana.editor.dtd.parsing.ast.DTDNodeType;
 import com.aptana.parsing.ast.IParseNode;
 
@@ -20,12 +21,200 @@ public class DTDParserTests extends TestCase
 		
 		IParseNode root = this.parse(
 			source,
-			DTDNodeType.ELEMENT_DECLARATION
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.EMPTY
 		);
 		
-		DTDElementDeclarationNode elementDecl = (DTDElementDeclarationNode) root.getFirstChild();
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
 		
 		assertEquals("svg", elementDecl.getName());
+	}
+	
+	/**
+	 * testAnyElement
+	 */
+	public void testAnyElement()
+	{
+		String source = "<!ELEMENT svg ANY>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ANY
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		
+		assertEquals("svg", elementDecl.getName());
+	}
+	
+	/**
+	 * testPCDataElement
+	 */
+	public void testPCDataElement()
+	{
+		String source = "<!ELEMENT svg (#PCDATA)>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.PCDATA
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		
+		assertEquals("svg", elementDecl.getName());
+	}
+	
+	/**
+	 * testZeroOrMorePCDataElement
+	 */
+	public void testZeroOrMorePCDataElement()
+	{
+		String source = "<!ELEMENT svg (#PCDATA)*>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ZERO_OR_MORE,
+			DTDNodeType.PCDATA
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		
+		assertEquals("svg", elementDecl.getName());
+	}
+	
+	/**
+	 * testZeroOrMorePCDataAndNamesElement
+	 */
+	public void testZeroOrMorePCDataAndNamesElement()
+	{
+		String source = "<!ELEMENT svg (#PCDATA circle)*>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ZERO_OR_MORE,
+			DTDNodeType.PCDATA,
+			DTDNodeType.ELEMENT
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		assertEquals("svg", elementDecl.getName());
+		
+		IParseNode kleene = elementDecl.getFirstChild();
+		assertNotNull(kleene);
+		
+		IParseNode element = kleene.getLastChild();
+		assertNotNull(element);
+		assertTrue(element instanceof DTDElementNode);
+		
+		assertEquals("circle", ((DTDElementNode) element).getName());
+	}
+	
+	/**
+	 * testSingleChild
+	 */
+	public void testSingleChild()
+	{
+		String source = "<!ELEMENT svg (circle)>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ELEMENT
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		assertEquals("svg", elementDecl.getName());
+		
+		IParseNode element = elementDecl.getFirstChild();
+		assertNotNull(element);
+		assertTrue(element instanceof DTDElementNode);
+		
+		assertEquals("circle", ((DTDElementNode) element).getName());
+	}
+	
+	/**
+	 * testZeroOrMoreSingleChild
+	 */
+	public void testZeroOrMoreSingleChild()
+	{
+		String source = "<!ELEMENT svg (circle)*>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ZERO_OR_MORE,
+			DTDNodeType.ELEMENT
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		assertEquals("svg", elementDecl.getName());
+		
+		IParseNode kleene = elementDecl.getFirstChild();
+		assertNotNull(kleene);
+		
+		IParseNode element = kleene.getFirstChild();
+		assertNotNull(element);
+		assertTrue(element instanceof DTDElementNode);
+		
+		assertEquals("circle", ((DTDElementNode) element).getName());
+	}
+	
+	/**
+	 * testOneOrMoreSingleChild
+	 */
+	public void testOneOrMoreSingleChild()
+	{
+		String source = "<!ELEMENT svg (circle)+>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.ONE_OR_MORE,
+			DTDNodeType.ELEMENT
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		assertEquals("svg", elementDecl.getName());
+		
+		IParseNode positive = elementDecl.getFirstChild();
+		assertNotNull(positive);
+		
+		IParseNode element = positive.getFirstChild();
+		assertNotNull(element);
+		assertTrue(element instanceof DTDElementNode);
+		
+		assertEquals("circle", ((DTDElementNode) element).getName());
+	}
+	
+	/**
+	 * testOptionalSingleChild
+	 */
+	public void testOptionalSingleChild()
+	{
+		String source = "<!ELEMENT svg (circle)?>";
+		
+		IParseNode root = this.parse(
+			source,
+			DTDNodeType.ELEMENT_DECLARATION,
+			DTDNodeType.OPTIONAL,
+			DTDNodeType.ELEMENT
+		);
+		
+		DTDElementDeclNode elementDecl = (DTDElementDeclNode) root.getFirstChild();
+		assertEquals("svg", elementDecl.getName());
+		
+		IParseNode optional = elementDecl.getFirstChild();
+		assertNotNull(optional);
+		
+		IParseNode element = optional.getFirstChild();
+		assertNotNull(element);
+		assertTrue(element instanceof DTDElementNode);
+		
+		assertEquals("circle", ((DTDElementNode) element).getName());
 	}
 	
 	/**
@@ -64,10 +253,12 @@ public class DTDParserTests extends TestCase
 		// make sure we got something
 		assertNotNull(result);
 		
+		IParseNode current = result;
+		
 		// check node types
 		for (DTDNodeType type : types)
 		{
-			IParseNode current = result.getNextNode();
+			current = current.getNextNode();
 		
 			assertNotNull(current);
 			assertEquals(type.getIndex(), current.getNodeType());
