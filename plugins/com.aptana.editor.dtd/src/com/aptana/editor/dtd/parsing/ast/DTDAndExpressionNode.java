bd1c07e41b1cea33ce709075aafd6db6623f0ce2
Kevin Lindsey <klindsey@aptana.com>
2010-10-06 15:31:07 -0500

    Add nodes for and- and or-expressions.

---
+++ plugins/com.aptana.editor.dtd/src/com/aptana/editor/dtd/parsing/ast/DTDAndExpressionNode.java
@@ -0,0 +1,12 @@
+package com.aptana.editor.dtd.parsing.ast;
+
+public class DTDAndExpressionNode extends DTDNode
+{
+	/**
+	 * DTDAndExpressionNode
+	 */
+	public DTDAndExpressionNode()
+	{
+		super(DTDNodeType.AND_EXPRESSION);
+	}
+}
---
+++ plugins/com.aptana.editor.dtd/src/com/aptana/editor/dtd/parsing/ast/DTDOrExpressionNode.java
@@ -0,0 +1,12 @@
+package com.aptana.editor.dtd.parsing.ast;
+
+public class DTDOrExpressionNode extends DTDNode
+{
+	/**
+	 * DTDOrExpressionNode
+	 */
+	public DTDOrExpressionNode()
+	{
+		super(DTDNodeType.OR_EXPRESSION);
+	}
+}
