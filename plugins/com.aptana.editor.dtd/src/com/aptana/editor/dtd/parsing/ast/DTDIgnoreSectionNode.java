81be2e463bee4a5950e3901eeb27ee1c51cd9132
Kevin Lindsey <klindsey@aptana.com>
2010-10-07 13:59:29 -0500

    Add new ignore section node and type

---
+++ plugins/com.aptana.editor.dtd/src/com/aptana/editor/dtd/parsing/ast/DTDIgnoreSectionNode.java
@@ -0,0 +1,12 @@
+package com.aptana.editor.dtd.parsing.ast;
+
+public class DTDIgnoreSectionNode extends DTDNode
+{
+	/**
+	 * DTDIncludeSectionNode
+	 */
+	public DTDIgnoreSectionNode()
+	{
+		super(DTDNodeType.IGNORE_SECTION);
+	}
+}
